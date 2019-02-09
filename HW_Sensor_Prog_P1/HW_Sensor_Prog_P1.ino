500ab73f1e68503c0542e4be7da3e8efda7dd3fb
Matthew Krugh <mkrugh@gmail.com>
2019-02-09 17:26:09 -0500

    Init HW_Sensor_Prog_P1

---
+++ HW_Sensor_Prog_P1/HW_Sensor_Prog_P1.ino
@@ -0,0 +1,27 @@
+/* 
+ *  Digital Manufacturing Homework Sensor Programming Part 1
+ *  Objective: Data collection and USB serial transmission
+ *  Required Hardware:
+ *  *   Teensy 3.2 
+ *  *   DS3231 RTC (I2C 0x68)
+ *  *   ADXL335 +/-3g Accelerometer
+ *  *   MPU-9250 IMU (I2C 0x69) - Requires AD0 to VDD jumper and SJ2 jumped left
+ *  Last updated Jan 2019
+ */
+#include<i2c_t3.h>
+#include "RTClib.h"
+#include "MPU9250.h"
+
+RTC_DS3231 rtc;
+MPU9250 myIMU;
+
+//Initialize variables here
+
+void setup() {
+  // Code placed here will run once on startup
+}
+
+void loop() {
+  // Code placed here will continuously loop after startup
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Digit_Manuf_HW
+# Digit_Manuf_HW  :paw_prints:
 Digital Manufacturing Homework Starter Code Repository
+Tested with:
+* Windows 10 x64 Build 17134, Ardunio IDE 1.8.8, Teensyduino 1.45
+* Ubuntu x64 18.04.1, Ardunio IDE 1.8.8, Teensyduino 1.45
+
+**Included folders:**
+- HW_Sensor_Prog-P1
+- HW_Sensor_Prog-P2
